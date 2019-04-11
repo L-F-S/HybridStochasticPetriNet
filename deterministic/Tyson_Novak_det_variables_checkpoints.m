39c8879a28a43323209ddeaf5571574998adb7c2
L-F-S <lorenzo.fsignorini@outlook.it>
2019-04-11 19:20:22 +0200

    Update Tyson_Novak_det_variables_checkpoints.m

--- deterministic/Tyson_Novak_det_variables_checkpoints.m
+++ deterministic/Tyson_Novak_det_variables_checkpoints.m
@@ -4,17 +4,21 @@
 %
 
 function[value,isterminal,direction] = Tyson_Novak_det_variables_checkpoints(t,x)
+
 keq=1000;
-%% variables
+
+% variables:
+
 m = x(1);
 CycBt= x(2);
 CKIt = x(7);
 Cyc_B = CycBt - 2*CycBt*CKIt/(CycBt + CKIt + keq^-1)*sqrt((CycBt + CKIt + keq^-1)^2 -4*CycBt*CKIt);
 
-%parameter
+% parameter Cyc_B_TH:
+
 Cyc_B_TH = 0.1; %Cyc_B threshold for cell division (Tyson and Novak, 2001)
 
-%CELL DIVISION TRIGGER
+% Cell division trigger:
 value = Cyc_B*m - Cyc_B_TH*m;
 isterminal = 1;
 direction = -1;
